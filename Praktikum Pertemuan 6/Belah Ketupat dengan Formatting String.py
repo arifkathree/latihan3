8ac97d438bf960eb5f25ff1af767268bf49e96ea
arifkathree <arifkatri378@gmail.com>
2022-10-28 03:49:41 +0700

    komit kedua

---
+++ Praktikum Pertemuan 6/Belah Ketupat dengan Formatting String.py
@@ -0,0 +1,12 @@
+print('Hello ini script Python')
+
+print('{0:>11}'.format('*'))
+print('{0:>12}'.format('***'))
+print('{0:>13}'.format('*****'))
+print('{0:>14}'.format('*******'))
+print('{0:>15}'.format('*********'))
+print('{0:>14}'.format('*******'))
+print('{0:>13}'.format('*****'))
+print('{0:>12}'.format('***'))
+print('{0:>11}'.format('*'))
+
---
+++ Praktikum Pertemuan 6/Penggabungan Penjumlahan dan Pembagian.py
@@ -0,0 +1,11 @@
+a=input("Masukkan nilai a:")
+b=input("Masukkan nilai b:")
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+
+# Konversi Nilai Variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%s".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%s".format(a,b) %(a/b))
